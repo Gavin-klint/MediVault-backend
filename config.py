1fe371a251f60f90db4a847376989a18c8574c7a
Abhay Pratap Singh <aps.8770724400@gmail.com>
2026-07-21 11:22:23 +0530

    Move secrets to environment variables
    Updated configuration to require environment variables for SECRET_KEY, DATABASE_URL, and ENCRYPTION_KEY, raising errors if they are not set.

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
 import os
 
 class Config:
-    # Generate a random secret key for session security
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-this-key'
-    
-    # CONNECTION STRING: mysql+pymysql://username:password@localhost/databasename
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://medivault_admin:Your_DB_Password@localhost/medivault_db'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    if not SECRET_KEY:
+        raise ValueError("SECRET_KEY environment variable not set")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    if not SQLALCHEMY_DATABASE_URI:
+        raise ValueError("DATABASE_URL environment variable not set")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    # Where we store the master encryption key (In real life, use a Key Vault)
-    ENCRYPTION_KEY = os.environ.get('ENCRYPTION_KEY') or b'Fua5_OMYbZYxxSWz6hINsCaIJ5U55r9jEPr9pkC-IvU='
+
+    ENCRYPTION_KEY = os.environ.get('ENCRYPTION_KEY')
+    if not ENCRYPTION_KEY:
+        raise ValueError("ENCRYPTION_KEY environment variable not set")
